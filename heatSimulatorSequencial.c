adb840af946be043bf76aa6f0e6d684ce37df64d
Rui <rui.filipe.machado@hotmail.com>
2016-10-31 18:04:41 +0000

    Add files via upload

---
+++ heatSimulatorSequencial.c
@@ -0,0 +1,178 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# include <time.h>
+# include <omp.h>
+
+# define M 200
+# define N 200
+# define N_THREADS 1
+
+
+
+/******************************************************************************/
+
+int main ( int argc, char *argv[] ){
+
+
+  
+  double diff;
+  float epsilon;
+  FILE *fp;
+  int i;
+  int iterations;
+  int iterations_print;
+  int j;
+  double mean;
+  char output_file[80];
+  int success;
+  double u[M][N];
+  double w[M][N];
+
+  omp_set_num_threads(N_THREADS);
+
+  printf ( "\n" );
+  printf ( "HEATED_PLATE\n" );
+  printf ( "  C version\n" );
+  printf ( "  A program to solve for the steady state temperature distribution\n" );
+  printf ( "  over a rectangular plate.\n" );
+  printf ( "\n" );
+  printf ( "  Spatial grid of %d by %d points.\n", M, N );
+/* 
+  Read EPSILON from the command line or the user.
+*/
+  
+  printf ( "\n" );
+  printf ( "  Enter EPSILON, the error tolerance:\n" );
+  success = scanf ( "%f", &epsilon );
+  
+  printf ( "\n" );
+  printf ( "  The iteration will be repeated until the change is <= %f\n", epsilon );
+  diff = epsilon;
+/* 
+  Read OUTPUT_FILE from the command line or the user.
+*/
+  printf ( "\n" );
+  printf ( "  Enter OUTPUT_FILE, the name of the output file:\n" );
+  success = scanf ( "%s", output_file );
+  
+  
+
+  if ( success != 1 )
+  {
+    printf ( "\n" );
+    printf ( "HEATED_PLATE\n" );
+    printf ( "  Error reading in the value of OUTPUT_FILE.\n");
+    return 1;
+  }
+
+  printf ( "\n" );
+  printf ( "  The steady state solution will be written to %s.\n", output_file );
+/* 
+  Set the boundary values, which don't change. 
+*/
+  for ( i = 1; i < M - 1; i++ )
+  {
+    w[i][0] = 0.0;
+  }
+  for ( i = 1; i < M - 1; i++ )
+  {
+    w[i][N-1] = 0.0;
+  }
+  for ( j = 0; j < N; j++ )
+  {
+    w[M-1][j] = 100.0;
+  }
+  for ( j = 0; j < N; j++ )
+  {
+    w[0][j] = 0.0;
+  }
+
+
+/* 
+  iterate until the  new solution W differs from the old solution U
+  by no more than EPSILON.
+*/
+  iterations = 0;
+  iterations_print = 1;
+  printf ( "\n" );
+  printf ( " Iteration  Change\n" );
+  printf ( "\n" );
+  double start = omp_get_wtime(); //inicio contagem do tempo
+
+  while ( epsilon <= diff )
+  {
+/*
+  Save the old solution in U.
+*/
+    for ( i = 0; i < M; i++ ) 
+    {
+      for ( j = 0; j < N; j++ )
+      {
+        u[i][j] = w[i][j];
+      }
+    }
+/*
+  Determine the new estimate of the solution at the interior points.
+  The new solution W is the average of north, south, east and west neighbors.
+*/
+    diff = 0.0;
+    for ( i = 1; i < M - 1; i++ )
+    {
+      for ( j = 1; j < N - 1; j++ )
+      {
+        w[i][j] = ( u[i-1][j] + u[i+1][j] + u[i][j-1] + u[i][j+1] ) / 4.0;
+
+        if ( diff < fabs ( w[i][j] - u[i][j] ) )
+        {
+          diff = fabs ( w[i][j] - u[i][j] );
+        }
+      }
+    }
+    iterations++;
+    if ( iterations == iterations_print )
+    {
+      printf ( "  %8d  %f\n", iterations, diff );
+      iterations_print = 2 * iterations_print;
+    }
+  } 
+ double end = omp_get_wtime();  //fim da contagem do tempo
+
+    
+  printf ( "\n" );
+  printf ( "  %8d  %f\n", iterations, diff );
+  printf ( "\n" );
+  printf ( "  Error tolerance achieved.\n" );
+  printf("Concluido com %d threads em %f segundos.\n", N_THREADS, (end-start));
+/*
+  Write the solution to the output file.
+*/
+  fp = fopen ( output_file, "w" );
+
+  fprintf ( fp, "%d\n", M );
+  fprintf ( fp, "%d\n", N );
+
+  for ( i = 0; i < M; i++ )
+  {
+    for ( j = 0; j < N; j++)
+    {
+      fprintf ( fp, "%6.2f ", w[i][j] );
+    }
+    fputc ( '\n', fp);
+  }
+  fclose ( fp );
+
+  printf ( "\n" );
+  printf ("  Solution written to the output file %s\n", output_file );
+/* 
+  All done! 
+*/
+  printf ( "\n" );
+  printf ( "HEATED_PLATE:\n" );
+  printf ( "  Normal end of execution.\n" );
+
+  return 0;
+
+
+}
+/******************************************************************************/
