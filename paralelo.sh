ee66a5119b6bbd35a6f1629b2d420ac22d1ec03a
Rui <rui.filipe.machado@hotmail.com>
2016-11-04 19:57:13 +0000

    Add files via upload

---
+++ paralelo.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+
+
+echo "System"
+echo ""
+echo "Processador - cat /proc/cpuinfo"
+echo ""
+cat /proc/cpuinfo
+echo ""
+echo "Processador - lscpu"
+echo ""
+lscpu
+echo ""
+echo "Memory"
+echo ""
+cat /proc/meminfo
+echo ""
+
+echo "REP 1"
+
+echo "1 thread"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 1
+
+echo ""
+echo "2 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 2
+
+echo ""
+echo "4 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 4
+
+echo ""
+echo "8 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 8
+
+echo ""
+echo "16 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 16
+
+echo ""
+echo "24 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 24
+
+echo ""
+echo "32 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 32
+
+echo ""
+echo "40 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 40
+
+echo ""
+echo "48 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 48
+
+echo "REP 2"
+
+echo "1 thread"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 1
+
+echo ""
+echo "2 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 2
+
+echo ""
+echo "4 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 4
+
+echo ""
+echo "8 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 8
+
+echo ""
+echo "16 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 16
+
+echo ""
+echo "24 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 24
+
+echo ""
+echo "32 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 32
+
+echo ""
+echo "40 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 40
+
+echo ""
+echo "48 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 48
+
+
+echo "REP 3"
+
+echo "1 thread"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 1
+
+echo ""
+echo "2 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 2
+
+echo ""
+echo "4 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 4
+
+echo ""
+echo "8 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 8
+
+echo ""
+echo "16 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 16
+
+echo ""
+echo "24 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 24
+
+echo ""
+echo "32 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 32
+
+echo ""
+echo "40 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 40
+
+echo ""
+echo "48 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 48
+
+echo "REP 4"
+
+echo "1 thread"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 1
+
+echo ""
+echo "2 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 2
+
+echo ""
+echo "4 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 4
+
+echo ""
+echo "8 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 8
+
+echo ""
+echo "16 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 16
+
+echo ""
+echo "24 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 24
+
+echo ""
+echo "32 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 32
+
+echo ""
+echo "40 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 40
+
+echo ""
+echo "48 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 48
+
+echo "REP 5"
+
+echo "1 thread"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 1
+
+echo ""
+echo "2 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 2
+
+echo ""
+echo "4 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 4
+
+echo ""
+echo "8 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 8
+
+echo ""
+echo "16 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 16
+
+echo ""
+echo "24 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 24
+
+echo ""
+echo "32 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 32
+
+echo ""
+echo "40 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 40
+
+echo ""
+echo "48 threads"
+~/pcp/pcpTrabalho1/heatSimulatorParalelo 0.001 testeParalelo.txt 48
+
